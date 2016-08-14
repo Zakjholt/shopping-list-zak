772d12e53a42a3795bfc177dc3053a0663f4c276
zak <zak.j.holt@gmail.com>
2016-08-14 16:28:47 -0400

    Checked the solution to optimize code
    I added the form reset, and used closest(“li).find(‘.shopping-item’)
    instead of .parent().prev()

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
     $('ul.shopping-list').append(
       '<li><span class="shopping-item">' + $("#shopping-list-entry").val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
     );
+    $(this)[0].reset();
   });
 
   $('.shopping-list').on('click', '.shopping-item-toggle', function() {
-    $(this).parent().prev().toggleClass('shopping-item__checked');
+    $(this).closest("li").find(".shopping-item").toggleClass('shopping-item__checked');
   });
 
 //**use the ul.on(click, li)** for listeners on li buttons
