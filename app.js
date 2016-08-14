a005b1ce449a0e00c16e5c8631e5fff4876ce2e3
zak <zak.j.holt@gmail.com>
2016-08-14 16:22:42 -0400

    functionally done

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ $(document).ready(function() {
     $('ul.shopping-list').append(
       '<li><span class="shopping-item">' + $("#shopping-list-entry").val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
     );
-    //append list item to bottom of ul.shopping-list
-    //add the contents of #shopping-list-entry to the li
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function() {
+    $(this).parent().prev().toggleClass('shopping-item__checked');
   });
 
 //**use the ul.on(click, li)** for listeners on li buttons
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function() {
+    $(this).closest('li').remove();
+  });
 
 });
